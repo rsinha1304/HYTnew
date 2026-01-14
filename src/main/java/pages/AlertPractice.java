ef5ddfeca8d809767d1b81a70678046fc16bb34f
rsinha1304 <rajsinha0104@gmail.com>
2026-01-14 22:52:10 +0530

    modify alert page

--- src/main/java/pages/AlertPractice.java
+++ src/main/java/pages/AlertPractice.java
@@ -1,9 +1,28 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class AlertPractice {
 
 	
 	//add new page and the element
+	
+	WebDriver driver;
+	
+	@FindBy(xpath="//button[@id='alertBox']")
+	private WebElement alertBox;
+	
+	
+	public AlertPractice(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void clickOnAlertBox() {
+		alertBox.click();
+		driver.switchTo().alert().accept();
+	}
 	
 	
 	
